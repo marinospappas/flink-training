7a4dabdd0a270fc62eaf86086214aa0279f59676
marinos <marinosch.99981@gmail.com>
2024-11-07 07:24:13 +0100

    Simplified consumer config

--- src/main/java/org/example/kafka/consumer/Consumer.java
+++ src/main/java/org/example/kafka/consumer/Consumer.java
@@ -2,13 +2,14 @@ package org.example.kafka.consumer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.example.txnprocessor.common.Alert;
 import org.example.txnprocessor.common.Transaction;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class ConsumerTransaction {
+public class Consumer {
 
     @KafkaListener(topics = "#{'${spring.kafka.topic-transaction}'}", containerFactory = "kafkaListenerContainerFactoryTransaction")
     public void listen(ConsumerRecord<Long, Transaction> message) {
@@ -23,5 +24,10 @@ public class ConsumerTransaction {
     @KafkaListener(topics = "#{'${spring.kafka.topic-transaction}'}", containerFactory = "kafkaListenerContainerFactoryTransaction")
     public void listen2(ConsumerRecord<Long, Transaction> message) {
         log.info("Consumer 2 - Received {} in group test: {} - {}, offset: {}", message.value().getClass().getName(), message.key(), message.value(), message.offset());
+    }
+
+    @KafkaListener(topics = "#{'${spring.kafka.topic-alert}'}", containerFactory = "kafkaListenerContainerFactoryAlert")
+    public void listenAlert(ConsumerRecord<String, Alert> message) {
+        log.info("Consumer Alert - Received {} in group test: {} - {}, offset: {}", message.value().getClass().getName(), message.key(), message.value(), message.offset());
     }
 }
